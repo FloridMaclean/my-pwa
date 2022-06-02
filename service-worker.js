1613af8059b421fcec1fd3973ebe8332fe3d8ee5
Florid Maclean <floridmaclean@gmail.com>
2022-06-02 01:13:49 -0400

    first commit

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-// Calle when the SW is installed
+// Every time something comes from the web
+self.addEventListener("fetch", function () {
+  return;
+});
+
+// Called when the SW is installed
 self.addEventListener("install", function (event) {
   console.log("Installed", event);
-//   self.skipWaiting();
+  //   self.skipWaiting();
 });
 
 // called when the SW is activated
 self.addEventListener("activate", function (event) {
   console.log("Activated", event);
-//   event.waitUntil(clients.claim());
-});
-
-// Every time something comes from the web
-self.addEventListener("fetch", function () {
-  return;
+  //   event.waitUntil(clients.claim());
 });
