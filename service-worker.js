e576ec358b1d6abf163846c08e8e8f31a7172ac0
Florid Maclean <floridmaclean@gmail.com>
2022-06-02 00:57:57 -0400

    try 1

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 // Calle when the SW is installed
 self.addEventListener("install", function (event) {
-  console.log("Installed!", event);
-  self.skipWaiting();
+  console.log("Installed", event);
+//   self.skipWaiting();
 });
 
 // called when the SW is activated
 self.addEventListener("activate", function (event) {
   console.log("Activated", event);
-  event.waitUntil(clients.claim());
+//   event.waitUntil(clients.claim());
 });
 
 // Every time something comes from the web
